b72b4e3dc6f23b9bc19fec217aab79022a502c53
wfarah <wael.a.farah@gmail.com>
2016-10-24 17:29:07 +1100

    Fix minor bug

--- server_mopsr_frb_detector.py
+++ server_mopsr_frb_detector.py
@@ -247,8 +247,8 @@ def candidateFilter(candidates,PulsarList,pulsar_file,threshold_filter=True):
 		for Pulsar in PulsarList:
 			if candidateIsPulsar(beam,H_dm,Pulsar):
 				pulsar_file.write("%s detected at fanbeam: %i with dm: %f "+\
-						"at time: %f and sample: %i\n",Pulsar['NAME'],beam,
-						H_dm,candidate['time'],candidate['sample'])
+						"at time: %f and sample: %i\n"%(Pulsar['NAME'],beam,\
+						H_dm,candidate['time'],candidate['sample']))
 				logging.info("%s detected at fanbeam: %i with dm: %f at time: %f and sample: %i",Pulsar['NAME'],
 						beam,H_dm,candidate['time'],candidate['sample'])
 				mask[i] = False
