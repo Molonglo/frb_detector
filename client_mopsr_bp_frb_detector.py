edacfde40c4e97ab738153eb7709ae385506189c
wfarah <wael.a.farah@gmail.com>
2016-10-25 22:15:46 +1100

    fix obs.header reading

--- client_mopsr_bp_frb_detector.py
+++ client_mopsr_bp_frb_detector.py
@@ -195,7 +195,9 @@ def process_candidate(in_queue,utc,source_name,rfi_writer_queue):
 						"sample: %i",beam,candidate['sample'])
 				if DUMP_VOLTAGES:
 					obs_header = parse_cfg(FIL_FILE_DIR+'/'+utc.value+'/'+\
-							'obs.header',['TSAMP'])
+							+source_name.value+\
+							'/BEAM_'+str(beam).zfill(3)+'/obs.header',\
+							['TSAMP'])
 					sampling_time = float(obs_header['TSAMP'])/10**6 # in seconds
 					send_dump_command(utc.value,sampling_time,
 							candidate,ftrs,proba)
